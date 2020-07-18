5019736a658434c2c340f1c1c80d9fb90211c670
cairneym <28552348+cairneym@users.noreply.github.com>
2020-07-18 20:54:10 +1000

    InstallSSMS.ps1
    Corrected check logic so that it actually does the install if not already there

--- DeployBaseline/InstallSSMS/installSSMS.ps1
+++ DeployBaseline/InstallSSMS/installSSMS.ps1
@@ -1,15 +1,11 @@
 # Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; 
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'));
+Set-ExecutionPolicy Bypass -Scope Process -Force
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install Software if it isn't already installed
 $AppPath = "C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\ssms.exe" 
 $IsInstalled = Test-Path $AppPath -PathType Leaf
 
-if ($IsInstalled) {
-    choco install sql-server-management-studio -y;
-    exit 0
-} else {
-    exit 1
-}
- 
+if (-not($IsInstalled)) {
+    choco install sql-server-management-studio -y
+} 
